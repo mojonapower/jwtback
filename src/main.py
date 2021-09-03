1d471e5d40f14aeaca42a681f4c6472e033468ac
mojonapower <maca95@gmail.com>
2021-09-03 02:00:22 +0000

    up back

--- src/main.py
+++ src/main.py
@@ -2,15 +2,20 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 import os
-from flask import Flask, request, jsonify, url_for
+from flask import Flask, request, jsonify, url_for, request
 from flask_migrate import Migrate
 from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
 from models import db, User
-#from models import Person
+#modulo para calcular el tiempo
+import datetime
+#pipenv install flask-jwt-extended
+#importar LO QUE NECESITAMOS DE JWT
+from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity 
 
+#
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DB_CONNECTION_STRING')
@@ -19,6 +24,9 @@ MIGRATE = Migrate(app, db)
 db.init_app(app)
 CORS(app)
 setup_admin(app)
+jwt = JWTManager(app)
+#
+
 
 # Handle/serialize errors like a JSON object
 @app.errorhandler(APIException)
@@ -30,14 +38,47 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
+@app.route('/user', methods=['GET','POST'])
 def handle_hello():
+    #cuando es un get conseguiremos todos los usuarios 
+    if request.method =='GET':
+        all_people = User.query.all()
+        all_people = list(map(lambda x: x.serialize(), all_people))
+    
+        return jsonify(all_people), 200
+    
+    else:
+        body = request.get_json() # obtener el request body de la solicitud
+        if body is None:
+            return "The request body is null", 400
+        if 'email' not in body:
+            return 'Especificar email', 400
+        if 'password' not in body:
+            return 'Especificar password',400
+        #estoy consultando si existe alguien con el email que mande en la api y consigo la primera coincidencia
+        onePeople = User.query.filter_by(email=body["email"]).first()
+        if onePeople:
+            if (onePeople.password == body["password"] ):
+                #CUANDO VALIDAMOS LA PASSWORD CREAREMOS EL TOKEN
+                expira = datetime.timedelta(minutes=2)
+                access_token = create_access_token(identity=onePeople.email, expires_delta=expira)
+                data = {
+                    "info_user": onePeople.serialize(),
+                    "token": access_token,
+                    "expires": expira.total_seconds()
+                }
+                return(jsonify(data))
+            else:
+                return(jsonify({"mensaje":False}))
+        else:
+            return(jsonify({"mensaje":"mail no se encuentra registrado"}))
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
-
-    return jsonify(response_body), 200
+@app.route("/privado", methods=['GET'])
+@jwt_required()
+def profile():
+    if request.method == 'GET':
+        token = get_jwt_identity()
+        return jsonify({"success": "Acceso a espacio privado", "usuario": token}), 200
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
